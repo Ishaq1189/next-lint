8577163b1cfca42911441de85eedb3b31371ceca
Ishaq <ishaq@dt.ae>
2021-05-31 22:27:23 +0400

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,51 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-    'prettier'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-  ],
-  'settings': {
-    'react': {
-      'version': 'latest',
+  plugins: ['react', 'simple-import-sort'],
+  settings: {
+    react: {
+      version: 'latest',
     },
   },
-  'rules': {
+  rules: {
+    'react/prop-types': 0,
+    'require-jsdoc': 0,
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'error',
+    'no-useless-escape': 'off',
+    'no-script-url': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-template-curly-in-string': 'off',
+    'react/prop-types': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'linebreak-style': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/no-danger': 0,
+    'react/forbid-prop-types': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+    'import/no-unresolved': 0,
+    'no-console': 0,
+    'react/no-multi-comp': 0,
+    'react/no-array-index-key': 0,
+    'no-unused-vars': 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'object-shorthand': 0,
+    'react-hooks/exhaustive-deps': 0,
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import React from "react";
+import Head from 'next/head';
+import React from 'react';
+
+import styles from '../styles/Home.module.css';
 
 /**
  * Home: The Landing page of the web app
@@ -20,8 +21,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -35,22 +35,16 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+            className={styles.card}>
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -59,10 +53,8 @@ export default function Home() {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          rel="noopener noreferrer">
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
